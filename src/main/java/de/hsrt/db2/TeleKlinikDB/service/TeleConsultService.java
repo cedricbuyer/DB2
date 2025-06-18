6b8055bb3796c5f7b7cf7e684ba5d4a40284eeda
Frederik Beimgraben <frederik@beimgraben.net>
2025-06-18 20:57:39 +0200

    feat!: Add Command Consumer

--- src/main/java/de/hsrt/db2/TeleKlinikDB/service/TeleConsultService.java
+++ src/main/java/de/hsrt/db2/TeleKlinikDB/service/TeleConsultService.java
@@ -1,15 +1,35 @@
 package de.hsrt.db2.TeleKlinikDB.service;
 
+import de.hsrt.db2.TeleKlinikDB.commands.TeleKlinikCommand;
+import de.hsrt.db2.TeleKlinikDB.commands.TeleKlinikContext;
+import de.hsrt.db2.TeleKlinikDB.model.GP;
+import de.hsrt.db2.TeleKlinikDB.model.Patient;
+import de.hsrt.db2.TeleKlinikDB.model.User;
 import de.hsrt.db2.TeleKlinikDB.repo.ChatRepo;
 import de.hsrt.db2.TeleKlinikDB.repo.MessageRepo;
+import de.hsrt.db2.TeleKlinikDB.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TeleConsultService {
-    @Autowired
-    public MessageRepo messageRepo;
+    @Autowired MessageRepo messageRepo;
+    @Autowired ChatRepo chatRepo;
+    @Autowired UserRepo<User> userRepo;
+    @Autowired UserRepo<GP> gpUserRepo;
+    @Autowired UserRepo<Patient> patientUserRepo;
 
-    @Autowired
-    public ChatRepo chatRepo;
+    public TeleKlinikContext getTeleKlinikContext() {
+        return new TeleKlinikContext(
+                userRepo,
+                gpUserRepo,
+                patientUserRepo,
+                messageRepo,
+                chatRepo
+        );
+    }
+
+    public void processTeleKlinikCommand(TeleKlinikCommand command) {
+        command.execute(getTeleKlinikContext());
+    }
 }
