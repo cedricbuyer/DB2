8af1bfc53b2999893b074f7f7d50c5a16209ba00
Frederik Beimgraben <frederik@beimgraben.net>
2025-06-25 15:00:50 +0200

    feat: Add a few filters

--- src/main/java/de/hsrt/db2/TeleConsultDB/TeleConsultDBApplication.java
+++ src/main/java/de/hsrt/db2/TeleConsultDB/TeleConsultDBApplication.java
@@ -3,7 +3,5 @@ package de.hsrt.db2.TeleConsultDB;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TeleConsultDBApplication {
-	public static void main(String[] args) { }
-}
+public class TeleConsultDBApplication { }
 
